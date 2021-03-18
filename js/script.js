54a36f05ea7ccb94065db0feb16bbf164cea56dc
rumanparvz <62352795+rumanparvz@users.noreply.github.com>
2021-03-18 14:15:32 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	// == owl carousel
+    $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+});
+})
